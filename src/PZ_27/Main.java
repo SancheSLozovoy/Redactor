2bd2a6fb34211fe1c34123ba7f2c4e9845fd3a70
Student <ann.ten.tkdlove@mail.ru>
2023-11-28 17:39:10 +0300

    Initial commit

---
+++ src/PZ_27/Main.java
@@ -0,0 +1,11 @@
+package PZ_27;
+
+public class Main {
+    public static void main(String[] args){
+        Redactor mainWindow = new Redactor();
+
+        mainWindow.pack();
+        mainWindow.setSize(800, 600);
+        mainWindow.setVisible(true);
+    }
+}
---
+++ src/PZ_27/Redactor.java
@@ -0,0 +1,16 @@
+package PZ_27;
+
+import javax.swing.*;
+
+public class Redactor extends JFrame{
+
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JButton открытьФайлButton;
+    private JButton сохранитьButton;
+    private JButton сохранитьКакButton;
+
+    public Redactor() {
+        this.getContentPane().add(panel1);
+    }
+}
