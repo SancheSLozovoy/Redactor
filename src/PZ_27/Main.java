3dc818b84915f7ea3a85a3230cc26fd9514a6f40
SancheSLozovoy <alex.lozovoy05@bk.ru>
2023-11-29 13:31:22 +0300

    com

--- src/PZ_27/Main.java
+++ src/PZ_27/Main.java
@@ -1,11 +1,13 @@
 package PZ_27;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args){
-        Redactor mainWindow = new Redactor();
-
-        mainWindow.pack();
-        mainWindow.setSize(800, 600);
-        mainWindow.setVisible(true);
+        Redactor red = new Redactor();
+        red.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        red.setSize(1000, 700);
+        red.setVisible(true);
     }
 }
--- src/PZ_27/Redactor.java
+++ src/PZ_27/Redactor.java
@@ -1,16 +1,72 @@
 package PZ_27;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class Redactor extends JFrame{
-
-    private JTextArea textArea1;
-    private JPanel panel1;
+public class Redactor extends JFrame {
     private JButton открытьФайлButton;
+    private JPanel panel1;
+    private JLabel открытФайлLabel;
     private JButton сохранитьButton;
-    private JButton сохранитьКакButton;
-
-    public Redactor() {
+    private JButton сокранитьКакButton;
+    private JTextArea textArea1;
+    public File file;
+    public Redactor(){
         this.getContentPane().add(panel1);
+
+        открытьФайлButton.addActionListener(e -> {
+            if(dialog(FileDialog.LOAD, "Открыть файл", "*.txt")){
+                StringBuilder s = new StringBuilder();
+                try(FileReader reader = new FileReader(file)){
+                    int c;
+                    while((c = reader.read()) != -1)
+                        s.append((char)c);
+                }catch (IOException ex){
+                    ex.printStackTrace();
+                }
+                textArea1.setText(s.toString());
+                открытФайлLabel.setText(file.getName());
+            }
+        });
+        сохранитьButton.addActionListener(e ->{
+            if(file != null) save();
+            else saveNew();
+        });
+        сокранитьКакButton.addActionListener(e ->{
+            saveNew();
+        });
+    }
+
+    private void saveNew(){
+        if(textArea1.getText().length() > 0)
+            if(dialog(FileDialog.SAVE, "Сохранить текстовый файл", "*.txt")){
+                save();
+                открытФайлLabel.setText(file.getName());
+            }
+    }
+
+    private void save(){
+        try{
+            FileWriter writer = new FileWriter(file);
+            writer.write(textArea1.getText());
+            writer.flush();
+            writer.close();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    private boolean dialog(int dialog, String title, String filter){
+        FileDialog fileDialog = new FileDialog(new Frame(), title, dialog);
+        fileDialog.setFile(filter);
+        fileDialog.setVisible(true);
+        File[] files = fileDialog.getFiles();
+        if(files.length > 0){
+            file = files[0];
+            return true;
+        }else return false;
     }
 }
